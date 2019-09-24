927728c349eccee73568d92428eac75b5b02d9e0
Mark <moleary1@wayfair.com>
2019-09-24 15:59:18 -0400

    returns data object, serializes to JSON

--- PubgStats/PubgStats/NetworkPubg.swift
+++ PubgStats/PubgStats/NetworkPubg.swift
@@ -8,10 +8,6 @@
 
 import Foundation
 
-//https://api.pubg.com/shards/steam/players?filter[playerNames]=MogMog
-//curl -X GET "https://api.pubg.com/shards/steam/players?filter[playerNames]=MogMog" -H "accept: application/vnd.api+json" -H "Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJlYzEyMTA2MC1iZDAyLTAxMzctNWE4Yi02OWIwNDIzNDczYzAiLCJpc3MiOiJnYW1lbG9ja2VyIiwiaWF0IjoxNTY4ODk0Mzk2LCJwdWIiOiJibHVlaG9sZSIsInRpdGxlIjoicHViZyIsImFwcCI6Im1hcmttb2xlYXJ5LWdtIn0.URs3K6ENnIKCQofOCvKJIecUk5GmFz-twmw2CzG2MMg"
-
-
 class NetworkPubg {
     let token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJlYzEyMTA2MC1iZDAyLTAxMzctNWE4Yi02OWIwNDIzNDczYzAiLCJpc3MiOiJnYW1lbG9ja2VyIiwiaWF0IjoxNTY4ODk0Mzk2LCJwdWIiOiJibHVlaG9sZSIsInRpdGxlIjoicHViZyIsImFwcCI6Im1hcmttb2xlYXJ5LWdtIn0.URs3K6ENnIKCQofOCvKJIecUk5GmFz-twmw2CzG2MMg"
     
@@ -23,12 +19,27 @@ class NetworkPubg {
         getRequest.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
         getRequest.setValue("application/vnd.api+json", forHTTPHeaderField: "accept")
         
-        let task = session.dataTask(with: getRequest, completionHandler: { data, response, error in
-        print(data)
-        print(response)
-        print(error)
-    })
-        
+        let task = session.dataTask(with: getRequest, completionHandler: { data, response, error -> Void in
+            if error != nil {
+                print("GET request: Communication Error: \(String(describing: error))")
+            }
+            
+            if data != nil {
+                do {
+                    let resultObject = try JSONSerialization.jsonObject(with: data!, options: []) as! [String:Any]
+                    print(resultObject)
+                } catch {
+                        print("Unable to parse JSON response")
+                    
+                }
+            } else {
+                    print("Received empty response.")
+            }
+        })
         task.resume()
-}
+    }
+    
+    
+    
+    
 }
---
+++ PubgStats/PubgStats/Player.swift
@@ -0,0 +1,15 @@
+//
+//  Stat.swift
+//  PubgStats
+//
+//  Created by Mark O'Leary on 9/19/19.
+//  Copyright © 2019 Mark O'Leary. All rights reserved.
+//
+
+import Foundation
+
+
+struct Player: Codable {
+    var name: String
+    var id: String
+}
